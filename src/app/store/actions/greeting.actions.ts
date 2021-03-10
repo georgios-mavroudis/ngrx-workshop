24b24263796dd1dd3a5e75f8b971a6a5b32f15a9
georgios-mavroudis <georgios@cardiologs.com>
2021-03-10 17:45:06 +0100

    Actions

--- src/app/store/actions/greeting.actions.ts
+++ src/app/store/actions/greeting.actions.ts
@@ -1,4 +1,11 @@
 import { createAction, props } from '@ngrx/store';
 import { Greeting } from '../reducers/greeting.reducer';
 
+export const sendGreeting = createAction(
+  '[Amb] sendGreeting',
+  props<{ greeting: Greeting }>(),
+);
 
+export const getGreeting = createAction(
+  '[Amb] getGreeting',
+);
