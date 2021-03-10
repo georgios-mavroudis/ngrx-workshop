e1f0adc56047dd09ddfc456e6290fcdbbb2dbd81
georgios-mavroudis <georgios@cardiologs.com>
2021-03-10 17:48:33 +0100

    Selectors

--- src/app/store/selectors/greeting.selectors.ts
+++ src/app/store/selectors/greeting.selectors.ts
@@ -1,2 +1,22 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { IGreetingState, selectGreetingEntities } from '../reducers/greeting.reducer';
 
+export const selectGreetingState = createFeatureSelector<IGreetingState>('greeting');
+
+const getGreetingEntities = createSelector(
+  selectGreetingState,
+  selectGreetingEntities,
+);
+
+export const getSpecificGreetingTimeSelector = (id: number) => createSelector(
+  selectGreetingState,
+  (state) => { console.log(state.entities); return state.entities[id]; },
+);
+
+export const getAllGreetingsSelector = createSelector(
+  getGreetingEntities,
+  (greetingEntities) => {
+    return Array.from(Object.values(greetingEntities));
+  },
+);
 
